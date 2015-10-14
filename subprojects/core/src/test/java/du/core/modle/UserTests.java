c285eedc64073eb451ad748c5019c5665d2f662b
DDQ <dudeqing@live.com>
2015-10-14 15:58:17 +0800

    分离

--- subprojects/core/src/test/java/du/core/modle/UserTests.java
+++ subprojects/core/src/test/java/du/core/modle/UserTests.java
@@ -3,18 +3,9 @@ package du.core.modle;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)  
-@WebAppConfiguration  
-@ContextConfiguration(locations = { "classpath:/spring/dispatcher-servlet.xml" }) 
 public class UserTests {
 
-	@Autowired
 	private SqlSessionFactory sqlSessionFactory;
 	
 	
